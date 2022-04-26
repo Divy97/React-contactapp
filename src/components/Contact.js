2748e7540794f340a01c6ae657dff6d9774f4606
Divy Parekh <divyparekh1810@gmail.com>
2022-04-26 16:15:20 +0530

    dispatch to update contact DONE

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,8 +59,12 @@ const Contact = ({ contact, contactKey }) => {
   // when the update icon/ pen ion is clicked
   const updateContact = () => {
     // dispatching one action to update contact
-    //TODO: use dispatch to update
-
+    //TODO: DONE use dispatch to update
+    dispatch({
+      type: CONTACT_TO_UPDATE,
+      payload:contact,
+      key: contactKey
+    })
     // and pushing to the add contact screen
     history.push("/contact/add");
   };
@@ -68,8 +72,11 @@ const Contact = ({ contact, contactKey }) => {
   // to view a single contact in the contact/view screen
   const viewSingleContact = contact => {
     // setting single contact in state
-    //TODO: use dispatch to view single contact
-
+    //TODO: DONE use dispatch to view single contact
+    dispatch({
+      type: SET_SINGLE_CONTACT,
+      payload:contact
+    })
     // sending...
     history.push("/contact/view");
   };
@@ -100,7 +107,7 @@ const Contact = ({ contact, contactKey }) => {
 
           <div className="text-secondary">{contact.phoneNumber}</div>
           <div className="text-secondary">
-            {/* {FIXME: display contact email}  */}
+            {contact.email}
           </div>
 
           <div className="text-info">{contact.address}</div>
